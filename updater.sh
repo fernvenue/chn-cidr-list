e94d7178428f00667f39dcb5e6dede8c65e531bd
fernvenue <git@fernvenue.com>
2021-09-17 06:41:51 +0000

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -9,7 +9,7 @@ git config --global user.email "$GITLAB_MAIL_ADDRESS"
 git config --global user.name "fernvenue"
 git clone git@gitlab.com:fernvenue/chn-cidr-list.git
 cd './chn-cidr-list'
-curl 'https://cdn.jsdelivr.net/gh/metowolf/iplist/data/special/china.txt' | grep ipv4 | grep CN | awk -F\| '{ printf("%s/%d\n", $4, 32-log($5)/log(2)) }' > ipv4.txt
+curl 'https://raw.githubusercontent.com/gaoyifan/china-operator-ip/ip-lists/china.txt' > ipv4.txt
 sed -i 's/[[:space:]]//g' './ipv4.txt'
 cp ./ipv4.txt ./ipv4.yaml
 sed -i "s|^|  - '&|g" ./ipv4.yaml
@@ -17,7 +17,7 @@ sed -i "s|$|&'|g" ./ipv4.yaml
 sed -i "1s|^|payload:\n|" ./ipv4.yaml
 cp ./ipv4.txt ./ipv4.conf
 sed -i "s|^|IP-CIDR,|g" ./ipv4.conf
-curl 'http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest' | grep ipv6 | grep CN | awk -F\| '{ printf("%s/%d\n", $4, 32-log($5)/log(2)) }' > ipv6.txt
+curl 'https://raw.githubusercontent.com/gaoyifan/china-operator-ip/ip-lists/china6.txt' > ipv6.txt
 sed -i 's/[[:space:]]//g' './ipv6.txt'
 cp ./ipv6.txt ./ipv6.yaml
 sed -i "s|^|  - '&|g" ./ipv6.yaml
