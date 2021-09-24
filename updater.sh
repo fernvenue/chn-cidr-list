6cf5a874a7fbfc08f4937a3a51587010e313365c
fernvenue <git@fernvenue.com>
2021-09-24 06:57:24 +0000

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -9,16 +9,25 @@ git config --global user.email "$GITLAB_MAIL_ADDRESS"
 git config --global user.name "fernvenue"
 git clone git@gitlab.com:fernvenue/chn-cidr-list.git
 cd './chn-cidr-list'
-curl 'https://raw.githubusercontent.com/gaoyifan/china-operator-ip/ip-lists/china.txt' > ipv4.txt
-sed -i 's/[[:space:]]//g' './ipv4.txt'
+curl 'https://raw.githubusercontent.com/gaoyifan/china-operator-ip/ip-lists/china.txt' > ipv4-bgp.txt
+sed -i 's/[[:space:]]//g' './ipv4-bgp.txt'
+curl 'http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest' | grep ipv4 | grep CN | awk -F\| '{ printf("%s/%d\n", $4, 32-log($5)/log(2)) }' > ipv4-apnic.txt
+sed -i 's/[[:space:]]//g' './ipv4-apnic.txt'
+wget `curl -s https://api.github.com/repos/zhanhb/cidr-merger/releases/latest | grep -Po '(?<=download_url\"\: \").*linux-amd64'`
+mv ./cidr-merger-linux-amd64 ./cidr-merger
+chmod +x ./cidr-merger
+cat ./ipv4*.txt | ./cidr-merger -s > ./ipv4.txt
 cp ./ipv4.txt ./ipv4.yaml
 sed -i "s|^|  - '&|g" ./ipv4.yaml
 sed -i "s|$|&'|g" ./ipv4.yaml
 sed -i "1s|^|payload:\n|" ./ipv4.yaml
 cp ./ipv4.txt ./ipv4.conf
 sed -i "s|^|IP-CIDR,|g" ./ipv4.conf
-curl 'https://raw.githubusercontent.com/gaoyifan/china-operator-ip/ip-lists/china6.txt' > ipv6.txt
-sed -i 's/[[:space:]]//g' './ipv6.txt'
+curl 'https://raw.githubusercontent.com/gaoyifan/china-operator-ip/ip-lists/china6.txt' > ipv6-bgp.txt
+sed -i 's/[[:space:]]//g' './ipv6-bgp.txt'
+curl 'http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest' | grep ipv6 | grep CN | awk -F\| '{ printf("%s/%d\n", $4, 32-log($5)/log(2)) }' > ipv6-apnic.txt
+sed -i 's/[[:space:]]//g' './ipv6-apnic.txt'
+cat ./ipv6*.txt | ./cidr-merger -s > ./ipv6.txt
 cp ./ipv6.txt ./ipv6.yaml
 sed -i "s|^|  - '&|g" ./ipv6.yaml
 sed -i "s|$|&'|g" ./ipv6.yaml
