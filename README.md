89a458f42b499f2b28ade2e7646660ae88008e48
fernvenue <git@fernvenue.com>
2022-02-06 22:25:31 +0000

    Update LICENSE, README.md, updater.sh, ipv4.txt, ipv4.conf, ipv4.yaml, ipv6.conf, ipv6.txt, ipv6.yaml, .gitlab-ci.yml

--- README.md
+++ README.md
@@ -2,12 +2,8 @@
 
 [![chn-cidr-list](https://img.shields.io/badge/LICENSE-BSD3%20Clause%20Liscense-red?style=flat-square)](./LICENSE)
 
-**Multi-format secondary distribution of China CIDR list.<br>**
+**Multi-format secondary distribution of China Mainland CIDR list.**
 
-Use CI/CD to automatically check and update, each update within one day.<br>
-Can be used to configure IP tables or connection rules.
-
-- https://github.com/gaoyifan/china-operator-ip/tree/ip-lists
-- http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest
-
-The original addresses are still good choices.
+Use CI/CD to automatically check and update, each update within **1 DAY**.  
+Can be used to config connection rules, support IPv4 and IPv6.  
+Source data from [BGP](https://github.com/gaoyifan/china-operator-ip/tree/ip-lists) and [APNIC](http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest).
--- updater.sh
+++ updater.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 eval $(ssh-agent -s)
-echo "$SSH_PRIVATE_KEY" | tr -d '\r' | ssh-add -
+echo "$SSH_PRIVATE_KEY" | tr -d "\r" | ssh-add -
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
 ssh-keyscan gitlab.com > ~/.ssh/known_hosts
-chmod 644 ~/.ssh/known_hosts
 git config --global user.email "$GITLAB_MAIL_ADDRESS"
 git config --global user.name "fernvenue"
 git clone git@gitlab.com:fernvenue/chn-cidr-list.git
-cd './chn-cidr-list'
-curl 'https://raw.githubusercontent.com/gaoyifan/china-operator-ip/ip-lists/china.txt' > ipv4-bgp.txt
-sed -i 's/[[:space:]]//g' './ipv4-bgp.txt'
-curl 'http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest' | grep ipv4 | grep CN | awk -F\| '{ printf("%s/%d\n", $4, 32-log($5)/log(2)) }' > ipv4-apnic.txt
-sed -i 's/[[:space:]]//g' './ipv4-apnic.txt'
+cd "./chn-cidr-list"
+curl "https://raw.githubusercontent.com/gaoyifan/china-operator-ip/ip-lists/china.txt" > ipv4-bgp.txt
+sed -i "s/[[:space:]]//g" "./ipv4-bgp.txt"
+curl "http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest" | grep ipv4 | grep CN | awk -F\| '{ printf("%s/%d\n", $4, 32-log($5)/log(2)) }' > ipv4-apnic.txt
+sed -i "s/[[:space:]]//g" "./ipv4-apnic.txt"
 wget `curl -s https://api.github.com/repos/zhanhb/cidr-merger/releases/latest | grep -Po '(?<=download_url\"\: \").*linux-amd64'`
 mv ./cidr-merger-linux-amd64 ./cidr-merger
 chmod +x ./cidr-merger
@@ -23,10 +22,10 @@ sed -i "s|$|&'|g" ./ipv4.yaml
 sed -i "1s|^|payload:\n|" ./ipv4.yaml
 cp ./ipv4.txt ./ipv4.conf
 sed -i "s|^|IP-CIDR,|g" ./ipv4.conf
-curl 'https://raw.githubusercontent.com/gaoyifan/china-operator-ip/ip-lists/china6.txt' > ipv6-bgp.txt
-sed -i 's/[[:space:]]//g' './ipv6-bgp.txt'
-curl 'http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest' | grep ipv6 | grep CN | awk -F\| '{ printf("%s/%d\n", $4, 32-log($5)/log(2)) }' > ipv6-apnic.txt
-sed -i 's/[[:space:]]//g' './ipv6-apnic.txt'
+curl "https://raw.githubusercontent.com/gaoyifan/china-operator-ip/ip-lists/china6.txt" > ipv6-bgp.txt
+sed -i "s/[[:space:]]//g" "./ipv6-bgp.txt"
+curl "http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest" | grep ipv6 | grep CN | awk -F\| '{ printf("%s/%d\n", $4, 32-log($5)/log(2)) }' > ipv6-apnic.txt
+sed -i "s/[[:space:]]//g" "./ipv6-apnic.txt"
 cat ./ipv6*.txt | ./cidr-merger -s > ./ipv6.txt
 cp ./ipv6.txt ./ipv6.yaml
 sed -i "s|^|  - '&|g" ./ipv6.yaml
@@ -37,5 +36,6 @@ sed -i "s|^|IP-CIDR,|g" ./ipv6.conf
 rm cidr-merger *bgp.txt *apnic.txt
 git init
 git add .
-git commit -m 'Update CIDR list'
+updated=`date --rfc-3339 sec`
+git commit -m "$updated"
 git push -u origin master
